49b7b4d5457678bb68ab26ac75ac5b7827f9b855
pachoulie <souy.angelina@gmail.com>
2017-01-08 17:31:31 +0100

    Added Task entity and controller

---
+++ src/AppBundle/Entity/Task.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use FOS\RestBundle\Validator\Constraints as Validator;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="task")
+ */
+class Task
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer")
+     *
+     * @var int
+     */
+    protected $id;
+
+    /**
+     * @ORM\Column(type="string")
+     *
+     * @var string
+     */
+    protected $text;
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getText()
+    {
+        return $this->text;
+    }
+
+    /**
+     * @param string $text
+     */
+    public function setText($text)
+    {
+        $this->text = $text;
+    }
+
+
+}
